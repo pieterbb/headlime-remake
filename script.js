7f3fea649cc647ae09f4a99d881072ccc8bf28f4
P <hello@piet.re>
2020-07-12 19:06:31 +0200

    init

---
+++ script.js
@@ -0,0 +1,208 @@
+//  Javascript Comment
+// Purpose is to put the sidebar value's in the text fields.
+
+// function update_style() {
+
+//   // Audience Keyword
+//   document.getElementById(""keyword_audience"").innerHTML = document.getElementById("
+//     "input_audience"
+//   ).value;
+// }
+
+function update_audience() {
+  //audience Keyword
+  for (item in document.getElementsByClassName("keyword_audience")){
+    item.innerHTML = document.getElementById("input_audience").value;
+    console.log("tes2t")
+    console.log(item)
+  }
+}
+
+function update_solution() {
+  //solution Keyword
+  document.getElementsByClassName(
+    "keyword_solution"
+  ).innerHTML = document.getElementById("input_solution").value;
+}
+
+function update_problem() {
+  //problem Keyword
+  document.getElementsByClassName(
+    "keyword_problem"
+  ).innerHTML = document.getElementById("input_problem").value;
+}
+
+function update_product() {
+  //product Keyword
+  document.getElementsByClassName(
+    "keyword_product"
+  ).innerHTML = document.getElementById("input_product").value;
+}
+
+function update_desired() {
+  //desired outcome Keyword
+  document.getElementsByClassName(
+    "keyword_desired"
+  ).innerHTML = document.getElementById("input_desired").value;
+}
+
+function update_period() {
+  //period Keyword
+  document.getElementsByClassName(
+    "keyword_period"
+  ).innerHTML = document.getElementById("input_period").value;
+}
+
+function update_authority() {
+  //authority Keyword
+  document.getElementsByClassName(
+    "keyword_authority"
+  ).innerHTML = document.getElementById("input_authority").value;
+}
+
+function update_number() {
+  //number Keyword
+  document.getElementsByClassName(
+    "keyword_number"
+  ).innerHTML = document.getElementById("input_number").value;
+}
+
+function update_product() {
+  //product name Keyword
+  document.getElementsByClassName(
+    "keyword_product"
+  ).innerHTML = document.getElementById("input_product").value;
+}
+
+function update_product() {
+  //product category Keyword
+  document.getElementsByClassName(
+    "keyword_product"
+  ).innerHTML = document.getElementById("input_product").value;
+}
+
+function update_undesirable() {
+  //undesirable outcome Keyword
+  document.getElementsByClassName(
+    "keyword_undesirable"
+  ).innerHTML = document.getElementById("input_undesirable").value;
+}
+
+function update_usp() {
+  //usp Keyword
+  document.getElementsByClassName(
+    "keyword_usp"
+  ).innerHTML = document.getElementById("input_usp").value;
+}
+
+function update_unique() {
+  //unique differentiator Keyword
+  document.getElementsByClassName(
+    "keyword_unique"
+  ).innerHTML = document.getElementById("input_unique").value;
+}
+
+function update_$() {
+  //$ Keyword
+  document.getElementsByClassName(
+    "keyword_$"
+  ).innerHTML = document.getElementById("input_$").value;
+}
+
+function update_current() {
+  //current solution Keyword
+  document.getElementsByClassName(
+    "keyword_current"
+  ).innerHTML = document.getElementById("input_current").value;
+}
+
+function update_do() {
+  //do this Keyword
+  document.getElementsByClassName(
+    "keyword_do"
+  ).innerHTML = document.getElementById("input_do").value;
+}
+
+function update_objection() {
+  //objection or anxiety Keyword
+  document.getElementsByClassName(
+    "keyword_objection"
+  ).innerHTML = document.getElementById("input_objection").value;
+}
+
+function update_topics() {
+  //topics Keyword
+  document.getElementsByClassName(
+    "keyword_topics"
+  ).innerHTML = document.getElementById("input_topics").value;
+}
+
+function update_doing() {
+  //doing common thing Keyword
+  document.getElementsByClassName(
+    "keyword_doing"
+  ).innerHTML = document.getElementById("input_doing").value;
+}
+
+function update_promise() {
+  //promise Keyword
+  document.getElementsByClassName(
+    "keyword_promise"
+  ).innerHTML = document.getElementById("input_promise").value;
+}
+
+function update_resource() {
+  //resource Keyword
+  document.getElementsByClassName(
+    "keyword_resource"
+  ).innerHTML = document.getElementById("input_resource").value;
+}
+
+function update_uncommon() {
+  //uncommon thing Keyword
+  document.getElementsByClassName(
+    "keyword_uncommon"
+  ).innerHTML = document.getElementById("input_uncommon").value;
+}
+
+function update_current() {
+  //current vendor Keyword
+  document.getElementsByClassName(
+    "keyword_current"
+  ).innerHTML = document.getElementById("input_current").value;
+}
+
+function update_superlative() {
+  //superlative Keyword
+  document.getElementsByClassName(
+    "keyword_superlative"
+  ).innerHTML = document.getElementById("input_superlative").value;
+}
+
+function update_topic() {
+  //topic Keyword
+  document.getElementsByClassName(
+    "keyword_topic"
+  ).innerHTML = document.getElementById("input_topic").value;
+}
+
+function update_objection() {
+  //objection Keyword
+  document.getElementsByClassName(
+    "keyword_objection"
+  ).innerHTML = document.getElementById("input_objection").value;
+}
+
+function update_comparative() {
+  //comparative Keyword
+  document.getElementsByClassName(
+    "keyword_comparative"
+  ).innerHTML = document.getElementById("input_comparative").value;
+}
+
+function update_competitor() {
+  //competitor Keyword
+  document.getElementsByClassName(
+    "keyword_competitor"
+  ).innerHTML = document.getElementById("input_competitor").value;
+}
